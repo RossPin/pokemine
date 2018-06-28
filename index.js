6f5edc27cec3c8b1f6af5a8d3b67a9891f1f7868
RossPin <dorpetz@hotmail.com>
2018-06-28 21:02:07 +1200

    faster but timesout if to many requests

--- index.js
+++ index.js
@@ -2,38 +2,39 @@ const request = require('superagent')
 const path = require('path')
 const fs = require('fs')
 
-let number = 4
+let number = 70
 let count = 0
-let pokemon = []
+let pokemonList = []
 
-getList().then(getPokemon)
+getList().then(() =>{
+  pokemonList.forEach(pokemon => getPokemon(pokemon))
+})
 
 function getList() {
     return request
         .get('https://pokeapi.co/api/v2/pokemon/?limit='+number)
         .then(res => {           
             let list = res.body.results            
-            pokemon = list.map(poke => {return {name: poke.name}})          
+            pokemonList = list.map(pokemon => {return {name: pokemon.name}})          
         }) 
 }
 
-function getPokemon(i) {
-    i = i || 0
-    return request
-        .get('https://pokeapi.co/api/v2/pokemon/'+pokemon[i].name)
-        .then(res => {
-            console.log(res.body.name)          
+function getPokemon(pokemon) {    
+    request
+        .get('https://pokeapi.co/api/v2/pokemon/'+pokemon.name)
+        .then(res => {                      
             let types = res.body.types.map(x => x.type.name)
-            pokemon[i].types = types
-            console.log(i+1)
-            if (i >= pokemon.length-1) writeJson()
-            else getPokemon(i+1)
+            pokemon.types = types
+            count++
+            console.log(count)
+            if (count === number) writeJson()
+            
         })
 }
 
 function writeJson(){
     const filepath = path.join(__dirname, 'pokemon.json')
-    fs.writeFile(filepath, JSON.stringify(pokemon, null, 2), 'utf-8', function(err) {
+    fs.writeFile(filepath, JSON.stringify(pokemonList, null, 2), 'utf-8', function(err) {
     if (err) throw err
     console.log('JSON written')
   })
