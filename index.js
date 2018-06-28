616613cb8d827cef2a19a92561dce06d198768ae
reubenjh <rjharcourt@gmail.com>
2018-06-28 20:10:45 +1200

    variable to  set number of pokemon

--- index.js
+++ index.js
@@ -2,30 +2,30 @@ const request = require('superagent')
 const path = require('path')
 const fs = require('fs')
 
+let number = 4
 let count = 0
 let pokemon = []
 
-getList()
+getList().then(getPokemon)
 
 function getList() {
     return request
-        .get('https://pokeapi.co/api/v2/pokemon/?limit=151')
+        .get('https://pokeapi.co/api/v2/pokemon/?limit='+number)
         .then(res => {           
             let list = res.body.results            
-            pokemon = list.map(x => {return {name: x.name}})            
-            getPokemon(0)
+            pokemon = list.map(poke => {return {name: poke.name}})          
         }) 
 }
 
 function getPokemon(i) {
+    i = i || 0
     return request
         .get('https://pokeapi.co/api/v2/pokemon/'+pokemon[i].name)
         .then(res => {
-            console.log(res.body.name)            
-            let type = res.body.types
-            let typesArr = type.map(x => x.type.name)
-            pokemon[i].types = typesArr
-            console.log(i)
+            console.log(res.body.name)          
+            let types = res.body.types.map(x => x.type.name)
+            pokemon[i].types = types
+            console.log(i+1)
             if (i >= pokemon.length-1) writeJson()
             else getPokemon(i+1)
         })
@@ -35,7 +35,7 @@ function writeJson(){
     const filepath = path.join(__dirname, 'pokemon.json')
     fs.writeFile(filepath, JSON.stringify(pokemon, null, 2), 'utf-8', function(err) {
     if (err) throw err
-    console.log('done')
+    console.log('JSON written')
   })
 }
 
